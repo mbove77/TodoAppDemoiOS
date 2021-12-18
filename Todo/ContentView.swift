850899780e750072611562b0eca267744895e7f1
Martin Bove <martin@fanalitica.com>
2021-12-18 15:56:11 -0300

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Resonant Sports on 18/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Resonant Sports on 18/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
